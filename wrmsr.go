45be8b150ee516a103fcf21326c8c309c8e7ca63
Michal Gorlas <michal.gorlas@9elements.com>
2026-02-13 23:00:40 +0100

    feat: add logic for wrmsr
    Signed-off-by: Michal Gorlas <michal.gorlas@9elements.com>

--- wrmsr.go
+++ wrmsr.go
@@ -1,1 +1,32 @@
 package msr
+
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+func (d MSRDev) Write(reg int64, val uint64) error {
+	regBuf := make([]byte, 8)
+
+	binary.NativeEndian.PutUint64(regBuf, val)
+
+	count, err := d.dev.WriteAt(regBuf, reg)
+	if err != nil {
+		return err
+	}
+
+	if count != 8 {
+		return fmt.Errorf("Write count not a uint64: %d", count)
+	}
+
+	return nil
+}
+
+func WriteMSR(cpu int, msr int64, val uint64) error {
+	err := MSR(cpu, func(dev MSRDev) error {
+		err := dev.Write(msr, val)
+		return err
+	})
+
+	return err
+}
